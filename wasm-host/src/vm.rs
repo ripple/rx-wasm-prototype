f8c4799cd20a391678626f9e519855081e3733f2
Peng Wang <pwang200@gmail.com>
2025-01-27 15:23:37 -0500

    ledger sqn host function

--- wasm-host/src/vm.rs
+++ wasm-host/src/vm.rs
@@ -1,56 +1,47 @@
-use wasmedge_sdk::{params, Vm, WasmVal, WasmEdgeResult, AsInstance};
+use std::collections::HashMap;
+use wasmedge_sdk::{params, Vm, WasmEdgeResult, AsInstance, Store, Module, Instance, CallingFrame,
+                   WasmValue, ImportObjectBuilder};
+use wasmedge_sdk::error::CoreError;
+use wasmedge_sdk::vm::SyncInst;
+use wasmedge_sdk::wasi::WasiModule;
 
-pub fn run_func<T: wasmedge_sdk::vm::SyncInst>(
-    vm : &mut Vm<T>,
+#[derive(Clone, Debug)]
+struct LedgerData {
+    sqn: i32,
+}
+
+fn get_ledger_sqn(
+    data: &mut LedgerData,
+    _inst: &mut Instance,
+    _caller: &mut CallingFrame,
+    _input: Vec<WasmValue>,
+) -> Result<Vec<WasmValue>, CoreError> {
+    Ok(vec![WasmValue::from_i32(data.sqn)])
+}
+
+pub fn run_func(
+    wasm_lib_file: &str,
     func_name: impl AsRef<str>,
-    tx_json: Vec<u8>,
-    lo_json: Vec<u8>,
-) -> WasmEdgeResult<(bool, String)> {
+) -> WasmEdgeResult<bool> {
+    let mut wasi_module = WasiModule::create(None, None, None).unwrap();
 
-    let tx_size = tx_json.len() as i32;
-    let tx_pointer = match vm.run_func(None, "allocate", params!(tx_size)) {
-        Ok(res) => res[0].to_i32(),
-        Err(err) => {
-            return Err(err);
-        }
-    };
-    println!("host tx alloc {} {}", tx_pointer, tx_size);
+    let ledger = LedgerData { sqn: 5 };
 
-    let lo_size = lo_json.len() as i32;
-    let lo_pointer = match vm.run_func(None, "allocate", params!(lo_size)) {
-        Ok(res) => res[0].to_i32(),
-        Err(err) => {
-            return Err(err);
-        }
-    };
-    println!("host lo alloc {} {}", lo_pointer, lo_size);
+    let mut import_builder = ImportObjectBuilder::new("host_lib", ledger)?;
+    import_builder
+        .with_func::<(), i32>("get_ledger_sqn", get_ledger_sqn)
+        .unwrap();
+    let mut import_object = import_builder.build();
 
-    {
-        let mut memory = vm.active_module_mut().unwrap().get_memory_mut("memory")?;
-        memory.set_data(tx_json, tx_pointer as u32).unwrap();
-        memory.set_data(lo_json, lo_pointer as u32).unwrap();
-    }
+    // let mut instances = HashMap::new();
+    let mut instances: HashMap<String, &mut dyn SyncInst> = HashMap::new();
+    instances.insert(wasi_module.name().to_string(), wasi_module.as_mut());
+    instances.insert(import_object.name().unwrap(), &mut import_object);
 
-    let rets = vm.run_func(None, func_name, params!(tx_pointer, tx_size, lo_pointer, lo_size))?;
+    let mut vm = Vm::new(Store::new(None, instances).unwrap());
+    let wasm_module = Module::from_file(None, &wasm_lib_file).unwrap();
+    vm.register_module(None, wasm_module.clone()).unwrap();
 
-    {
-        let mut memory = vm.active_module_mut().unwrap().get_memory_mut("memory")?;
-        let outter = rets[0].to_i32();
-        let rvec = memory.get_data(outter as u32, 9)?;
-        //TODO self.free(None, params!(rets[0].to_i32(), 9))?;
-
-        let flag = rvec[0];
-        let ret_pointer = i32::from_le_bytes(rvec[1..5].try_into().unwrap());
-        let ret_len = i32::from_le_bytes(rvec[5..9].try_into().unwrap());
-        println!("host run_func {} {} {}", flag, ret_pointer, ret_len);
-
-        let memory = vm.active_module_mut().unwrap().get_memory_mut("memory")?;
-        let rvec = memory.get_data(ret_pointer as u32, ret_len as u32)?;
-        let fn_re = String::from_utf8(rvec).unwrap();
-        println!("host run_func {:?}", fn_re);
-
-        vm.run_func(None, "deallocate", params!(outter, 9))?;
-        vm.run_func(None, "deallocate", params!(ret_pointer, ret_len))?;
-        Ok((flag == 1, fn_re))
-    }
+    let rets = vm.run_func(None, func_name, params!())?;
+    Ok(rets[0].to_i32() == 1)
 }
--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -1,74 +1,11 @@
-use std::{mem, u32};
-// use std::str::FromStr;
-use serde_json::Value;
-
 #[no_mangle]
-pub extern fn allocate(size: usize) -> *mut u8 {
-    let mut buffer = Vec::with_capacity(size);
-    let pointer = buffer.as_mut_ptr();
-    mem::forget(buffer);
-    pointer
+pub fn ready() -> bool {
+    unsafe { host_lib::get_ledger_sqn() >= 5}
 }
 
-#[no_mangle]
-pub extern fn deallocate(pointer: *mut u8, capacity: usize) {
-    unsafe {
-        // println!("deallocate {:?}", pointer);
-        let _ = Vec::from_raw_parts(pointer, 0, capacity);
+pub mod host_lib {
+    #[link(wasm_import_module = "host_lib")]
+    extern "C" {
+        pub fn get_ledger_sqn() -> i32;
     }
 }
-
-#[no_mangle]
-pub extern fn compare_accountID(tx_json_ptr: *mut u8, tx_json_size: usize, lo_json_ptr: *mut u8, lo_json_size: usize)
-                                -> *mut u8
-{
-    let tx_data;
-    let lo_data;
-    unsafe {
-        tx_data = Vec::from_raw_parts(tx_json_ptr, tx_json_size, tx_json_size);
-        lo_data = Vec::from_raw_parts(lo_json_ptr, lo_json_size, lo_json_size);
-    }
-
-    let tx_json_value: Value = serde_json::from_slice(tx_data.as_slice()).unwrap();
-    let lo_json_value: Value = serde_json::from_slice(lo_data.as_slice()).unwrap();
-    let tx_account = tx_json_value.get("Account").unwrap();
-    let lo_account = lo_json_value.get("Account").unwrap();
-
-    let data_field = lo_json_value.get("Data").unwrap();
-    let data = data_field.as_str().unwrap();
-    let mut counter = u32::from_str_radix(data, 10).unwrap();
-
-    let good =
-        if tx_account == lo_account {
-            counter = counter - 1;
-            if counter == 0 {
-                1
-            } else {
-                0
-            }
-        } else {
-            0
-        };
-    println!("test good = {:?}, counter = {}", good, counter);
-
-    // pack data
-    let mut buffer: Vec<u8> = Vec::with_capacity(9);
-    buffer.push(good);
-    {
-        let data = u32::to_string(&counter);
-        let pointer = data.as_ptr();
-        let mut pointer_u32 = (pointer as u32).to_le_bytes().to_vec();
-        let pointer_u32_len = pointer_u32.len();
-        buffer.append(&mut pointer_u32);
-        let len = data.len() as u32;
-        let mut len_u32 = len.to_le_bytes().to_vec();
-        buffer.append(&mut len_u32);
-        println!("test data {}, pointer = {:?}, pointer_u32_len = {}, len = {}",
-                 data, pointer as u32, pointer_u32_len, len);
-        mem::forget(data);
-    }
-    let pointer = buffer.as_mut_ptr();
-    mem::forget(buffer);
-    pointer
-}
-
