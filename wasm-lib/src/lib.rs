5ee66118ad99128e3b7c96a4f8e7c1e10c792501
Peng Wang <pwang200@gmail.com>
2025-03-21 14:40:26 -0400

    use new host function name

--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -1,11 +1,11 @@
 #[no_mangle]
 pub fn ready() -> bool {
-    unsafe { host_lib::get_ledger_sqn() >= 5}
+    unsafe { host_lib::getLedgerSqn() >= 5}
 }
 
 pub mod host_lib {
     #[link(wasm_import_module = "host_lib")]
     extern "C" {
-        pub fn get_ledger_sqn() -> i32;
+        pub fn getLedgerSqn() -> i32;
     }
 }
